0d2097a80aec288a7e6ca87c32999279e016d5e1
Agnieszka Drabczyk <adrabczyk90@gmail.com>
2021-10-06 19:54:25 +0200

    adding working navigations with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
-  { path: 'home', component: AppComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
