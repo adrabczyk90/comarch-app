08c5ad16a3b615b70176d200438db8d097065592
Agnieszka Drabczyk <adrabczyk90@gmail.com>
2021-10-07 10:37:06 +0200

    adding form with buttons ang mat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ViewPostsComponent } from './posts/view-posts/view-posts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule,} from '@angular/material/select';
+
 
 @NgModule({
   declarations: [
@@ -23,7 +29,12 @@ import { ViewPostsComponent } from './posts/view-posts/view-posts.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   exports: [ RouterModule ],
   providers: [],
